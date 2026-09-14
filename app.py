e54148c6e474549a24ab510985cc419a6b8f6133
Ali Muti Ur Rehman <alimutiurrehman@gmail.com>
2026-09-14 13:53:26 +0500

    Added Task Creation endpoint

--- app.py
+++ app.py
@@ -1,7 +1,29 @@
 from fastapi import FastAPI
+from pydantic import BaseModel
 
 app=FastAPI()
+
+class TaskCreate(BaseModel):
+    title:str
+
+
+tasks=[]
+next_id=1
 
 @app.get("/")
 def home():
     return {"Message":"TODO API is running"}
+
+@app.post("/tasks")
+def add_task(task:TaskCreate):
+    global next_id
+
+    new_task={
+        "id":next_id,
+        "title":task.title,
+        "done": False
+    }
+
+    tasks.append(new_task)
+    next_id+=1
+    return new_task
